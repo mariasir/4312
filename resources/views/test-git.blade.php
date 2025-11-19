61e6b79c2ae46780554811d05f8914ef81296750
mariasir <maria.sirca@blue-soft.com>
2025-11-19 23:44:09 +0200

    test 2

---
+++ resources/views/test-git.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="ro">
+<head>
+    <meta charset="UTF-8">
+    <title>Test Git Deploy 4312</title>
+</head>
+<body>
+<h1>Merge deploy-ul cu Git 🎉</h1>
+<p>Pagina asta este servită din view-ul <strong>test-git.blade.php</strong>.</p>
+<p>Subdomeniu: <strong>4312.blue-soft.ro</strong></p>
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('test-git');
 });
+
+/*Route::get('/test-git', function () {
+    return view('test-git');
+});*/
